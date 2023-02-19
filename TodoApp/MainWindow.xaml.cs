b91d2878c4289c36bc9c91cc470acd8d4fde4d13
SokolovAndr <korch45@yandex.ru>
2023-02-19 15:36:12 +0300

    Добавьте файлы проекта.

---
+++ TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using TodoApp.Models;
+using TodoApp.Services;
+
+namespace TodoApp
+{
+
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<TodoModel> _todoDataList; //нижнее подчеркивание для private поля
+        private FileIOService _fileIOService;  
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOService(PATH);
+
+            try 
+            {
+                _todoDataList = _fileIOService.LoadData();
+            }
+            catch (Exception ex) //если возникает ошибка
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+
+
+            DataGridTest.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+        }
+
+        private void _todoDataList_ListChanged(object? sender, ListChangedEventArgs e)
+        {
+            if(e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex) //если возникает ошибка
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+
+            }
+        }
+    }
+}
---
+++ TodoApp/Models/TodoModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    public class TodoModel : INotifyPropertyChanged
+    {
+        private bool _IsDone;
+        private string _text;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public bool IsDone
+        {
+            get { return _IsDone; }
+            set 
+            {
+                if (_IsDone == value)
+                    return;
+                _IsDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+        public string Text
+        {
+            get { return _text; }
+            set 
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));  //PropertyChanged?.Invoke - означает провреку на null
+
+            #region
+            //ТО ЖЕ САМОЕ, провека на null
+            //if (PropertyChanged != null)
+            //{
+            //    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            //}
+            #endregion
+        }
+    }
+}
